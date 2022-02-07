270f579ec193e75f065220ce2e85adc8afaafe9a
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-02-08 00:09:22 +0100

    Add display stocks cards

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import './stock.css';
 
 const Stock = (stock) => {
   const {
     name, symbol, price, change, percentage,
   } = stock;
+  const changeFloat = parseFloat(change).toFixed(2);
+  const percentageFloat = parseFloat(percentage).toFixed(2);
   return (
-    <div>
-      <h3>{name}</h3>
-      <h4>{symbol}</h4>
-      <p>{price}</p>
-      <p>{change}</p>
-      <p>{percentage}</p>
+    <div className="card">
+      <h4>{name}</h4>
+      <h5>
+        Symbol:
+        {' '}
+        {symbol}
+      </h5>
+      <p>
+        Price: $
+        {price}
+      </p>
+      <p>
+        Change:
+        {' '}
+        <span style={{ color: (changeFloat > 0) ? 'black' : 'red' }}>
+          {changeFloat}
+        </span>
+      </p>
+      <p>
+        Percent change:
+        {' '}
+        <span style={{ color: (percentageFloat > 0) ? 'black' : 'red' }}>
+          {percentageFloat}
+          %
+        </span>
+      </p>
     </div>
   );
 };
--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import Stock from './Stock';
+import './stocks.css';
 
 const Stocks = () => {
   const stocks = useSelector((state) => state.stocksReducer);
@@ -9,19 +10,21 @@ const Stocks = () => {
   console.log(stocks);
   return (
     <div>
-      <h2>Most active US stocks</h2>
-      {stocks.map(
-        (stock) => (
-          <Stock
-            key={uuidv4()}
-            name={stock.company_name}
-            symbol={stock.symbol}
-            price={stock.price}
-            change={stock.change}
-            percentage={stock.change_percentage}
-          />
-        ),
-      )}
+      <h2 className="most-heading">Most active US stocks</h2>
+      <section className="card-container">
+        {stocks.map(
+          (stock) => (
+            <Stock
+              key={uuidv4()}
+              name={stock.company_name}
+              symbol={stock.symbol}
+              price={stock.price}
+              change={stock.change}
+              percentage={stock.change_percentage}
+            />
+          ),
+        )}
+      </section>
     </div>
   );
 };
