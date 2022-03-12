78f43ff655efcda2f43e6ff7cd0aa5715e1ed446
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-03-12 11:47:13 +0100

    Changing expired API key

--- src/redux/StocksFetch.jsx
+++ src/redux/StocksFetch.jsx
@@ -1,7 +1,7 @@
 import { getStocks } from './StocksReducer';
 
 const URL = 'https://api.finage.co.uk/market-information/us/most-actives';
-const apiKey = 'API_KEY873FXAPMWP2TQJQDYXKAKBTRNM4VM7U3';
+const apiKey = 'API_KEY71UYHHN4T6NYZY7718KF12JOV7B5NNJ6';
 
 const StocksFetch = () => async (dispatch) => {
   const res = await fetch(`${URL}?apikey=${apiKey}`);
