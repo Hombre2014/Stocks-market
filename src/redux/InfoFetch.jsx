e2a0c81715a5f554bd85cde8e053ff66c762ec99
Hombre2014 <chamkoriyski@gmail.com>
2022-05-01 13:03:43 +0200

    Chaning API key

--- src/redux/InfoFetch.jsx
+++ src/redux/InfoFetch.jsx
@@ -1,7 +1,7 @@
 import { getInfo } from './InfoReducer';
 
 const URL = 'https://api.finage.co.uk/detail/stock';
-const apiKey = process.env.REACT_APP_FINAGE_API_KEY;
+const apiKey = 'API_KEY97TABP515TS1B1P74MYG1H55N6FU0YLD';
 
 const InfoFetch = (payload) => async (dispatch) => {
   const res = await fetch(`${URL}/${payload}?apikey=${apiKey}`);
--- src/redux/StocksFetch.jsx
+++ src/redux/StocksFetch.jsx
@@ -1,7 +1,7 @@
 import { getStocks } from './StocksReducer';
 
 const URL = 'https://api.finage.co.uk/market-information/us/most-actives';
-const apiKey = process.env.REACT_APP_FINAGE_API_KEY;
+const apiKey = 'API_KEY97TABP515TS1B1P74MYG1H55N6FU0YLD';
 
 const StocksFetch = () => async (dispatch) => {
   const res = await fetch(`${URL}?apikey=${apiKey}`);
